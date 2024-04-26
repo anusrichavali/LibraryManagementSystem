4daabbeb5044849e6078a2aa91d345d3d05c7904
Anusri Chavali <chavali.anusri@gmail.com>
2024-04-25 23:10:27 -0700

    Library branches and borrower table CRUD operations

--- library_management_api.py
+++ library_management_api.py
@@ -152,7 +152,142 @@ def delete_book_copy(book_id):
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+#CRUD operations on Library Branches
+@app.route('/LibraryBranches', methods = ['POST'])
+def add_branch():
+    try:
+        data = request.json
+        branch_id = data['branch_id']
+        branchName = data['branch name']
+        branchAddress = data['branch address']
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        connection = connect_database()
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO LibraryBranches (branch_id, branchName, branchAddress) VALUES (%s, %s, %s)', (branch_id, branchName, branchAddress))
+        connection.commit()
+        connection.close()
+        return jsonify({'message': 'Branch added successfully'}), 201
+    except Exception as e:
+        return jsonify({'error': str(e) + ' is missing'}), 500
+
+@app.route('/LibraryBranches', methods=['GET'])
+def get_branches():
+    try:
+        connection = connect_database()
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM LibraryBranches')
+        branches = cursor.fetchall()
+        connection.close()
+        return jsonify(branches), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@app.route('/LibraryBranches/<int:branch_id>', methods = ['PUT'])
+def update_branch(branch_id):
+    try:
+        data = request.get_json()
+        categories_to_update = data.keys()
+        columns = ['branchName', 'branchAddress']
+        for i in categories_to_update:
+            if i not in columns:
+                raise Exception("Invalid column name provided.")
+
+        connection = connect_database()
+        cursor = connection.cursor()
+        if 'branchName' in categories_to_update:
+            branchName = data['branchName']
+            cursor.execute("Update LibraryBranches SET branchName = (%s) WHERE branch_id = (%s)", (branchName, branch_id))
+        if 'branchAddress' in categories_to_update:
+            branchAddress = data['branchAddress']
+            cursor.execute("Update LibraryBranches SET branchAddress = (%s) WHERE branch_id = (%s)", (branchAddress, branch_id))
+        connection.commit()
+        connection.close()
+        return jsonify({'message': 'branch successfully updated'}), 201
+    except Exception as ex:
+        return jsonify({'error in branch update': str(ex)}), 500
+    
+@app.route('/LibraryBranches/<int:branch_id>', methods=['DELETE'])
+def delete_branch(branch_id):
+    try:
+        conn = connect_database()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM LibraryBranches WHERE branch_id = %s', (branch_id,))
+        conn.commit()
+        conn.close()
+        return jsonify({'message': 'Branch deleted successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+#CRUD operations on borrowers
+@app.route('/Borrowers', methods = ['POST'])
+def add_borrower():
+    try:
+        data = request.json
+        borrower_id = data['borrower_id']
+        fullName = data['full name']
+        address = data['address']
+        card_no = data['card number']
+        phone_no = data['phone number']
+
+        connection = connect_database()
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO Borrowers (borrower_id, fullName, address, card_no, phone_no) VALUES (%s, %s, %s, %s, %s)', (borrower_id, fullName, address, card_no, phone_no))
+        connection.commit()
+        connection.close()
+        return jsonify({'message': 'Borrower added successfully'}), 201
+    except Exception as e:
+        return jsonify({'error': str(e) + ' is missing'}), 500
+
+@app.route('/Borrowers', methods=['GET'])
+def get_borrowers():
+    try:
+        connection = connect_database()
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM Borrowers')
+        branches = cursor.fetchall()
+        connection.close()
+        return jsonify(branches), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@app.route('/Borrowers/<int:borrower_id>', methods = ['PUT'])
+def update_borrower(borrower_id):
+    try:
+        data = request.get_json()
+        categories_to_update = data.keys()
+        columns = ['fullName', 'address', 'phone_no']
+        for i in categories_to_update:
+            if i not in columns:
+                raise Exception("Invalid column name provided.")
+
+        connection = connect_database()
+        cursor = connection.cursor()
+        if 'fullName' in categories_to_update:
+            fullName = data['full name']
+            cursor.execute("Update Borrowers SET fullName = (%s) WHERE borrower_id = (%s)", (fullName, borrower_id))
+        if 'address' in categories_to_update:
+            address = data['address']
+            cursor.execute("Update Borrowers SET address = (%s) WHERE borrower_id = (%s)", (address, borrower_id))
+        if 'phone_no' in categories_to_update:
+            phone_no = data['phone_no']
+            cursor.execute("Update Borrowers SET phone_no = (%s) WHERE borrower_id = (%s)", (phone_no, borrower_id))
+        connection.commit()
+        connection.close()
+        return jsonify({'message': 'borrower information successfully updated'}), 201
+    except Exception as ex:
+        return jsonify({'error in task update': str(ex)}), 500
+    
+@app.route('/Borrowers/<int:borrower_id>', methods=['DELETE'])
+def delete_borrower(borrower_id):
+    try:
+        conn = connect_database()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM Borrowers WHERE borrower_id = %s', (borrower_id,))
+        conn.commit()
+        conn.close()
+        return jsonify({'message': 'Borrower deleted successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+app.run(debug=True)
 
